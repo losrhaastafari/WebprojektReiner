e82446a6d718b6a23aed17660c54176535d4ead8
losrhaastafari <rhaastafari@legionofsensei.de>
2025-02-01 18:37:07 +0100

    Aufgabe 2, Teil 1 erledigt

--- server.js
+++ server.js
@@ -1,24 +1,27 @@
 import Fastify from "fastify";
-import { CustomerRoutes } from "./routing/routing_customers.js";
+import fastifyMultipart from "@fastify/multipart";
+import { CustomerRoutes } from "./routing/routing_customers.js"; // Korrigiere den Pfad hier
+import { OfferRoutes } from "./routing/routing_offers.js";
+import { FileRoutes } from "./routing/routing_files.js";
+import dbConnector from "./database/database.js";
 import { customerSchema } from "./schemas/customer.schema.js";
 import { offerSchema } from "./schemas/offer.schema.js";
-import { OfferRoutes } from "./routing/routing_offers.js";
-import dbConnector from "./database/database.js";
 
 const fastify = Fastify({
     logger: true
 });
 
+fastify.register(fastifyMultipart);
 fastify.addSchema(customerSchema);
-fastify.register(CustomerRoutes, { prefix: "/Customer"})
+fastify.register(CustomerRoutes, { prefix: "/Customer" });
 fastify.register(dbConnector);
 fastify.addSchema(offerSchema);
-fastify.register(OfferRoutes, { prefix: "/Offer"});
+fastify.register(OfferRoutes, { prefix: "/Offer" });
+fastify.register(FileRoutes, { prefix: "/Offer" });
 
 try {
-    await fastify.listen({ port: 8080});
-}
-catch (err) {
+    await fastify.listen({ port: 8080 });
+} catch (err) {
     fastify.log.error(err);
     process.exit(1);
 }
